ba08a61614c316ea5f19f0f5ba2f101736033c7a
mechztcc <dojathan@gmail.com>
2022-02-21 10:02:52 -0300

    feat: added  hash password to create user service

--- src/modules/user/services/CreateUserService.ts
+++ src/modules/user/services/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getCustomRepository } from 'typeorm';
 import { User } from '../typeorm/entities/User';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { hash} from 'bcryptjs';
+import AppError from '@shared/errors/AppError';
 
 interface IRequest {
   name: string;
@@ -9,10 +11,22 @@ interface IRequest {
 }
 
 export class CreateUserService {
+
+  constructor() {
+  }
+
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
 
-		const user = usersRepository.create({ name, email, password });
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+		const user = await usersRepository.create({ name, email, password: hashedPassword });
 		await usersRepository.save(user);
 
 		return user;
--- src/modules/user/typeorm/repositories/UsersRepository.ts
+++ src/modules/user/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,10 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "../entities/User";
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-
+  async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({ where: { email }})
+    return user;
+  }
 }
