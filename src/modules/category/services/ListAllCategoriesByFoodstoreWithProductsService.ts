01fa218102eeeddc9c61fb36b063f3013f1cd5fb
mechztcc <dojathan@gmail.com>
2022-04-02 15:32:40 -0300

    fix: refactoring code

--- src/modules/category/services/ListAllCategoriesByFoodstoreWithProductsService.ts
+++ src/modules/category/services/ListAllCategoriesByFoodstoreWithProductsService.ts
@@ -1,4 +1,5 @@
 import { FoodstoreRepository } from '@modules/foodstore/typeorm/repositories/FoodstoreRepository';
+import { Product } from '@modules/product/typeorm/entities/Product';
 import { getCustomRepository } from 'typeorm';
 import { Category } from '../typeorm/entities/Category';
 import { CategoryRepository } from '../typeorm/repositories/CategoryRepositories';
@@ -8,7 +9,7 @@ interface IRequest {
 }
 
 export class ListAllCategoriesByFoodstoreWithProducts {
-  async execute({ id }: IRequest): Promise<Category[]> {
+  async execute({ id }: IRequest): Promise<Product[]> {
     const categoryRepository = getCustomRepository(CategoryRepository);
     const foodstoreRepository = getCustomRepository(FoodstoreRepository);
 
@@ -17,9 +18,25 @@ export class ListAllCategoriesByFoodstoreWithProducts {
     const categories = await categoryRepository.find({
       where: { foodstore: foodstore },
       select: ['name'],
-	    relations: ['product']
+      relations: ['product'],
     });
+    console.log(categories[0].product);
 
-    return categories;
+    let prods: any[] = [];
+
+    for (let index = 0; index < categories.length; index++) {
+      const element = categories[index];
+
+      let { product } = element;
+
+      if(product.length > 0) {
+        for (let index = 0; index < product.length; index++) {
+          const el = product[index];
+          prods.push(el);
+        }
+      }
+    }
+
+    return prods;
   }
 }
